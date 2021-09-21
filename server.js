761cd09a0b47eb09ab727c692647f9e04db62ea2
Zachary Boelter <zboelter@gmail.com>
2021-09-21 16:38:42 -0500

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//set up express boiler
+// set up express boiler
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -14,7 +14,26 @@ app.get('/tip/:total/:tippercentage', (req, res) => {
     res.send(`Total tip is ${totalTip}`);
 });
 
+// set array to variable
+let magicBall = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
 
+// create two variable
+app.get('/magic/:input', (req, res) => {
+    // input in url    
+    let question = (`${req.params.input}`);
+    // random string by length    
+    let answer = (magicBall[Math.floor(Math.random() * magicBall.length)]);
+    // plug into html on page
+    res.send(`<html>
+        <body>
+            <h1> What is your question? <br> 
+            ${question}? 
+            <br>
+            Magic 8 Ball says... <br>
+             ${answer} </h1>
+        </body>
+    </html>`);
+});
 
 
 
