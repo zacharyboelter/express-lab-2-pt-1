bedd8a554a0e753de798eb257ba1c116f0ac54bf
Zachary Boelter <zboelter@gmail.com>
2021-09-21 15:58:12 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+//set up express boiler
 const express = require('express');
 const app = express();
 const port = 3000;
 
-
-
-
+// same as the lab earlier
 app.get('/greeting/:name', (req, res) => {
     res.send(`Hello ${req.params.name}`);
 });
+
+// calculate tip set to a variable and run maths through res.send. cross fingers.
+app.get('/tip/:total/:tippercentage', (req, res) => {
+    let totalTip = (parseInt(req.params.total) * (parseInt(req.params.tippercentage) / 100));
+    res.send(`Total tip is ${totalTip}`);
+});
+
 
 
 
