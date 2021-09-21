1e8f2e615e71ea469e351661587edd2b2ce5a357
Zachary Boelter <zboelter@gmail.com>
2021-09-21 15:24:08 -0500

    Greeting express application created.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+
+app.get('/greeting/:name', (req, res) => {
+    res.send(`Hello ${req.params.name}`);
+});
+
+
+
+
+app.listen(port, () => {
+    console.log('listening on port', port);
+});
